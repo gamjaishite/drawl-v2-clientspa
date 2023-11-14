055d53cd24bfd00720e53bc12a818a28d7623978
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2023-11-14 09:46:09 +0700

    feat: add button functionality

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -8,37 +8,65 @@ import {
   } from "@/components/ui/table"
 
   import { ButtonIconCheck, ButtonIconCross } from "../buttons/Icon";
+  import React, { useRef, useState } from 'react'
+  import { Check, X } from 'lucide-react';
 
-const button_collection = (
-  <div>
-    <ButtonIconCheck />
-    <ButtonIconCross />
-  </div>
+  import { Button } from "@/components/ui/button"
+
+
+// const button_collection = (
+//   <div>
+//     <ButtonIconCheck  />
+//     <ButtonIconCross />
+//   </div>
   
-);
+// );
    
-  const user_data = [
-    {
-      user_id: "1",
-      username: "breezy",
-      email: "x1@gmail.com",
-      buttons: button_collection,
-      user_link: "https://lucide.dev/icons/x",
-    },
-    {
-      user_id: "2",
-      username: "breezy232",
-      email: "x2@gmail.com",
-      buttons: button_collection,
-      user_link: "https://lucide.dev/icons/x",
-    },
-   
-  ]
+  
 
   
    
   export function TableUser() {
+    const user_data = [
+      {
+        user_id: 1,
+        username: "breezy",
+        email: "x1@gmail.com",
+        user_link: "https://lucide.dev/icons/x",
+      },
+      {
+        user_id: 2,
+        username: "breezy232",
+        email: "x2@gmail.com",
+        user_link: "https://lucide.dev/icons/x",
+      },
+     
+    ]
+
+    const [lists, setList] = useState(user_data)
+    const [count, setCount] = useState(3)
+    const [updateState, setUpdateState] = useState(-1)
+
+    function handleAccept(id: any, e: any) {
+      e.preventDefault();
+      setCount(count + 1);
+      const newUsers = lists.concat([{
+        user_id: count,
+        username: "breezy232",
+        email: "x2@gmail.com",
+        user_link: "https://lucide.dev/icons/x",
+      }])
+      setList(newUsers);
+    }
+
+    function handleReject(id: any, e: any) {
+      e.preventDefault();
+      setList(lists => lists.filter((li) => li.user_id !== id));
+    }
+
+
     return (
+      
       <Table>
         <TableHeader>
           <TableRow>
@@ -49,7 +77,7 @@ const button_collection = (
           </TableRow>
         </TableHeader>
         <TableBody>
-          {user_data.map((user) => (
+          {lists.map((user) => (
             <TableRow key={user.user_id}>
               <TableCell className="font-medium">{user.user_id}</TableCell>
               <TableCell>
@@ -58,32 +86,63 @@ const button_collection = (
                 </a>
                 </TableCell>
               <TableCell>{user.email}</TableCell>
-              <TableCell className="text-right">{user.buttons}</TableCell>
+              <TableCell className="text-right">
+                <div>
+                  <Button variant="outline" size="icon" onClick={(e) => handleAccept(user.user_id, e)}>
+                    <Check className="h-4 w-4" />
+                  </Button>
+                  <Button variant="outline" size="icon" onClick={(e) => handleReject(user.user_id, e)}>
+                    <X className="h-4 w-4" />
+                  </Button>
+                </div>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
+
     )
   }
 
+  
+export function TableCatalog() {
+
   const catalog_data = [
     {
-      catalog_id: "1",
+      catalog_id: 1,
       title: "jujutsu kaisen",
       description: "Jujutsu Kaisen is a Japanese manga series written and illustrated by Gege Akutami. It has been serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump since March 2018",
-      buttons: button_collection,
       catalog_link: "https://en.wikipedia.org/wiki/Jujutsu_Kaisen",
     },
     {
-      catalog_id: "2",
+      catalog_id: 2,
       title: "demon slayer",
       description: "Demon Slayer: Kimetsu no Yaiba is a Japanese manga series written and illustrated by Koyoharu Gotouge. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump",
-      buttons: button_collection,
       catalog_link: "https://en.wikipedia.org/wiki/Demon_Slayer:_Kimetsu_no_Yaiba",
     },
   ]
 
-export function TableCatalog() {
+
+  const [lists, setList] = useState(catalog_data)
+    const [count, setCount] = useState(3)
+    const [updateState, setUpdateState] = useState(-1)
+
+    function handleAccept(id: any) {
+      setCount(count + 1);
+      const newlist = lists.concat({
+        catalog_id: count,
+        title: "breezy232",
+        description: "x2@gmail.com",
+        catalog_link: "https://lucide.dev/icons/x",
+      })
+      setList(newlist)
+    }
+
+    function handleReject(id: any) {
+      const newlist = lists.filter((li) => li.catalog_id !== id)
+      setList(newlist)
+    }
+
   return (
     <Table>
         <TableHeader>
@@ -95,16 +154,25 @@ export function TableCatalog() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {catalog_data.map((catalog) => (
+          {lists.map((catalog) => (
             <TableRow key={catalog.catalog_id}>
               <TableCell className="font-medium">{catalog.catalog_id}</TableCell>
               <TableCell>
-                <a href={catalog.catalog_id} target="_blank" rel="noopener noreferrer">
+                <a href={catalog.catalog_link} target="_blank" rel="noopener noreferrer">
                 {catalog.title}
                 </a>
                 </TableCell>
               <TableCell>{catalog.description}</TableCell>
-              <TableCell className="text-right">{catalog.buttons}</TableCell>
+              <TableCell className="text-right">
+              <div>
+                  <Button variant="outline" size="icon" onClick={() => handleAccept(catalog.catalog_id)}>
+                    <Check className="h-4 w-4" />
+                  </Button>
+                  <Button variant="outline" size="icon" onClick={() => handleReject(catalog.catalog_id)}>
+                    <X className="h-4 w-4" />
+                  </Button>
+                </div>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -112,25 +180,46 @@ export function TableCatalog() {
   )
 }  
 
-const report_data = [
-  {
-    username: "breezy",
-    email: "x1@gmail.com",
-    report_description: "xxx",
-    buttons: button_collection,
-    user_link: "https://lucide.dev/icons/x",
-  },
-  {
-    username: "breezy232",
-    email: "x2@gmail.com",
-    report_description: "xxx",
-    buttons: button_collection,
-    user_link: "https://lucide.dev/icons/x",
-  },
- 
-]
+
 
 export function TableReport() {
+
+  const report_data = [
+    {
+      username: "breezy",
+      email: "x1@gmail.com",
+      report_description: "xxx",
+      user_link: "https://lucide.dev/icons/x",
+    },
+    {
+      username: "breezy232",
+      email: "x2@gmail.com",
+      report_description: "xxx",
+      user_link: "https://lucide.dev/icons/x",
+    },
+   
+  ]
+
+  const [lists, setList] = useState(report_data)
+    const [count, setCount] = useState(2)
+    const [updateState, setUpdateState] = useState(-1)
+
+    function handleAccept(email: any) {
+      setCount(count + 1);
+      const newlist = lists.concat({
+        username: "breezy232",
+        email: "x"+ count + "@gmail.com",
+        report_description: "xxx",
+        user_link: "https://lucide.dev/icons/x",
+      })
+      setList(newlist)
+    }
+
+    function handleReject(email: any) {
+      const newlist = lists.filter((li) => li.email !== email)
+      setList(newlist)
+    }
+
   return (
     <Table>
       <TableHeader>
@@ -142,7 +231,7 @@ export function TableReport() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {report_data.map((report) => (
+        {lists.map((report) => (
           <TableRow key={report.email}>
             <TableCell className="font-medium">
               <a href={report.user_link} target="_blank" rel="noopener noreferrer">
@@ -153,7 +242,16 @@ export function TableReport() {
               {report.email}
               </TableCell>
             <TableCell>{report.report_description}</TableCell>
-            <TableCell className="text-right">{report.buttons}</TableCell>
+            <TableCell className="text-right">
+            <div>
+                  <Button variant="outline" size="icon" onClick={() => handleAccept(report.email)}>
+                    <Check className="h-4 w-4" />
+                  </Button>
+                  <Button variant="outline" size="icon" onClick={() => handleReject(report.email)}>
+                    <X className="h-4 w-4" />
+                  </Button>
+                </div>
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
--- src/pages/Dashboard/verifycatalog.tsx
+++ src/pages/Dashboard/verifycatalog.tsx
@@ -1,6 +1,17 @@
 import { TableCatalog, } from "@/components/table/Table";
+import {useCookies} from 'react-cookie'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import {useEffect, useState} from 'react'
+
+
+
 
 const VerifyCatalog = () => {
+  const [cookies] = useCookies()
+  const [isRefetch, setIsRefetch] = useState(false)
+
+  
+
     return (
       <>
         <main>Berikut adalah tabel untuk mem-verify catalog:</main>
